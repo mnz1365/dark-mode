ad018f7643706d64113d7c7154dd7f38c4685e5b
mnz <mzarei297@gmail.com>
2025-01-04 09:03:49 +0330

    first commint

---
+++ code.js
@@ -0,0 +1,10 @@
+function myLight() {
+    document.querySelector('h1').style.color = 'black';
+    document.body.style.backgroundColor = 'white';
+}
+
+function myDark() {
+    document.querySelector('h1').style.color = 'white';
+    document.body.style.backgroundColor = 'black';
+
+}
